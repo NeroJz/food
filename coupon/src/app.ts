c5c61213e07ac38239fbaf5fad45221d5b35217c
KumHoe <nerojz86@gmail.com>
2023-07-29 14:05:23 +0800

    [coupon] Added json and cookieSession setup

--- coupon/src/app.ts
+++ coupon/src/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: true,
+}));
 
 app.get('/api/coupon', (req, res) => {
   res.send('hello');
