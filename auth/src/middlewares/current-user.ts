94e8129c8ee387a67eff2742efdfd5c99fe9a98e
KumHoe <nerojz86@gmail.com>
2023-08-31 21:05:56 +0800

    [auth] Added current-user middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  email: string;
+  id: string;
+}
+
+declare global {
+  namespace Express {
+    export interface Request {
+      currentUser: UserPayload
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const currentUser = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+    req.currentUser = currentUser;
+  } catch (err) { }
+
+  next();
+};
