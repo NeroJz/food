cfd6475b295f395bb6dba87c9f4f3028c5bc7ae3
KumHoe <nerojz86@gmail.com>
2023-09-01 08:35:24 +0800

    [client] add placeholder attribute

--- food-client/src/app/share/input/input.component.ts
+++ food-client/src/app/share/input/input.component.ts
@@ -9,6 +9,7 @@ import { FormControl } from '@angular/forms';
 export class InputComponent {
   @Input() control!: FormControl;
   @Input() label!: string;
+  @Input() placeholder?: string = '';
 
   ngOnInit() { }
 }
