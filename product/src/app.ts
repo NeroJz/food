2fddf57867b84469465729f4bc77d6609120bb0e
KumHoe <nerojz86@gmail.com>
2023-09-18 23:06:08 +0800

    [product] add products router

--- product/src/app.ts
+++ product/src/app.ts
@@ -5,7 +5,8 @@ import cookieSession from 'cookie-session';
 import cors from 'cors';
 
 import { errorHandler } from './middlewares/error-handle';
-
+import { getProductsRouter } from './routes/products';
+import { getProductByIdRouter } from './routes/productById';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,9 +21,8 @@ app.use(cookieSession({
 app.use(cors());
 
 // Add Routes
-app.get('/api/product', (req, res) => {
-  res.send('Product API activated')
-});
+app.use(getProductsRouter);
+app.use(getProductByIdRouter);
 
 // Add Error Handling
 app.use(errorHandler);
